a06f93c580f6aa7ec74632ddba964c8baa95c77c
Ajay Tomar <ajaytomar4321@gmail.com>
2022-11-23 13:15:52 +0530

    final commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@=xsbdeliif#lr_u&=abo%w(9vxcngz+5=42g9q^v9lbsc4jb#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ajay-portfolio1.herokuapp.com/', '127.0.0.1' ]
 
 
 # Application definition
